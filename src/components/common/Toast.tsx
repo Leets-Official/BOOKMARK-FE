ec1aa47413be166a4fa59c1023a76b0a9a8e1da4
정찬원 <jcw0522@gachon.ac.kr>
2025-06-29 16:50:19 +0900

    chore : 주석 추가

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { tv } from 'tailwind-variants';
 
+// props 타입 정의
 export interface ToastProps {
   show: boolean;
   message: string;
   type?: 'positive' | 'error';
 }
 
+// tailwind-variants를 사용하여 toast 스타일을 구성
 const toast = tv({
+  // 기본 스타일 (항상 적용)
   base: [
     'fixed top-[80px] left-1/2 -translate-x-1/2 w-60 h-10 rounded-3xl bg-white',
     'flex items-center justify-center shadow-md',
     'text-base font-normal transition-opacity duration-300',
   ],
+  // 조건부 스타일 정의
   variants: {
     type: {
       positive: 'text-green-500 border border-green-500',
@@ -23,6 +27,7 @@ const toast = tv({
       false: 'opacity-0',
     },
   },
+  // 기본 variant 설정
   defaultVariants: {
     type: 'positive',
     show: false,
--- src/example/ToastExample.tsx
+++ src/example/ToastExample.tsx
@@ -4,10 +4,14 @@ import { useCallback, useState } from 'react';
 const ToastExample = () => {
   const [toast, setToast] = useState<ToastProps>({ show: false, type: 'error', message: '' });
 
+  /* 
+    타입과 메시지를 받아서 상태를 변경하는 함수
+    useCallback을 사용해서 showToast함수가 리렌더링 될 때마다 참조가 바뀌는 것을 방지
+  */
   const showToast = useCallback((type: 'positive' | 'error', message: string) => {
     setToast({ show: true, type, message });
     setTimeout(() => {
-      setToast((prev: any) => ({ ...prev, show: false }));
+      setToast((prev: any) => ({ ...prev, show: false })); // 2초 뒤에 show: false로 바꿔 토스트 숨기기
     }, 2000);
   }, []);
 
