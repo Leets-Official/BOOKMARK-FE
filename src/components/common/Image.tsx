393f79d4bec400243d6aa2edfb7b634e2870cf6a
tayiekang <taei518@naver.com>
2025-06-29 17:52:43 +0900

    feat: input/image 컴포넌트 구현

--- src/components/common/Image.tsx
+++ src/components/common/Image.tsx
@@ -1,5 +1,36 @@
-const Image = () => {
-  return <div></div>;
+import React from "react";
+
+interface ImageProps{
+  src: string;
+  alt?: string;             
+  width?: string;           
+  height?: string;           
+  rounded?: string;          
+  onClick?: () => void;
+}
+
+const Image: React.FC<ImageProps> = ({
+  src,
+  alt = "image",
+  width = "w-32",
+  height = "h-32",
+  rounded = "rounded-lg",
+  onClick,
+}) => {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      onClick={onClick}
+      className={`
+        ${width}
+        ${height}
+        object-cover
+        ${rounded}
+        ${onClick ? "cursor-pointer" : ""}
+      `}
+    />
+  );
 };
 
 export default Image;
--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,5 +1,32 @@
-const Input = () => {
-  return <div></div>;
+// components/input.tsx
+import React from 'react';
+
+interface InputProps {
+  placeholder?: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  fontSize?: string;
+  textColor?: string;
+  bgColor?: string;
+}
+
+const Input: React.FC<InputProps> = ({
+  placeholder = '',
+  value,
+  onChange,
+  fontSize = 'text-base',
+  textColor = 'text-black',
+  bgColor = 'bg-white',
+}) => {
+  return (
+    <input
+      type='text'
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className={`w-full px-3 py-2 border rounded-lg outline-none border-gray-300 ${fontSize} ${textColor} ${bgColor}`}
+    />
+  );
 };
 
 export default Input;
