443c7bcd49b7b3fec07fe5138248eaefa818d6fb
Donghyeon Shin <shindong0321@gmail.com>
2025-07-09 20:30:47 +0900

    feat : pages, compoents 폴더 및 파일 구조화

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { Home } from './pages';
+import { Home, Search, SignUp } from './pages';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+  },
+  {
+    path: '/search',
+    element: <Search />,
   },
 ]);
 
