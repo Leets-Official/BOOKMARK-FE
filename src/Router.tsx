889de39bc948b8b0b78dc11523a1c8e7265421d5
Donghyeon Shin <shindong0321@gmail.com>
2025-07-11 17:34:30 +0900

    build : react-device-detect 라이브러리 설치

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { Home, Search, SignUp } from './pages';
+import { Home, Search, SignUp, Save } from './pages';
 import App from './App';
 
 const router = createBrowserRouter([
@@ -18,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: 'search',
         element: <Search />,
+      },
+      {
+        path: 'save',
+        element: <Save />,
       },
     ],
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Home from './Home';
 import SignUp from './SignUp';
 import Search from './Search';
+import Save from './Save';
 
-export { Home, SignUp, Search };
+export { Home, SignUp, Search, Save };
