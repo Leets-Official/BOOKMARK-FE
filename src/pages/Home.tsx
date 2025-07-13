fba2cc7d8cec35d0943fcb2a6c031c153fc8d6cb
정찬원 <jcw0522@gachon.ac.kr>
2025-07-13 13:56:41 +0900

    chore: Home 컴포넌트 초기화

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,5 @@
-import Card from '@/components/ui/card/Card';
-import TestImage from '@/assets/test.jpg';
-
 const Home = () => {
-  const cardList = [
-    {
-      id: 1,
-      title: 'React Lifecycle 정리',
-      platform: '티스토리',
-      image: TestImage,
-      isLoading: false,
-    },
-    {
-      id: 2,
-      title: 'Vue3 Composition API',
-      platform: 'Velog',
-      image: TestImage,
-      isLoading: true,
-    },
-    {
-      id: 3,
-      title: 'Next.js App Router',
-      platform: 'GitHub Blog',
-      image: undefined,
-      isLoading: false,
-    },
-  ];
-  return (
-    <div className='flex flex-col items-center justify-center min-h-screen gap-6'>
-      {cardList.map((card) => (
-        <Card
-          key={card.id}
-          title={card.title}
-          platform={card.platform}
-          image={card.image}
-          isLoading={card.isLoading}
-        />
-      ))}
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
