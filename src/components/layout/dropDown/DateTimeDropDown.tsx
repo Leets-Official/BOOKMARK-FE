8d52c3620b1d50555a1426cf0f1a1bd98f067c1b
Donghyeon Shin <shindong0321@gmail.com>
2025-07-17 23:02:07 +0900

    feat : DateTimeDropDown layout 컴포넌트 구현

---
+++ src/components/layout/dropDown/DateTimeDropDown.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import DropDown from '@/components/ui/DropDown';
+import { motion } from 'framer-motion';
+import { BackArrowIcon } from '@/assets';
+
+interface DateTimeDropDownProps {
+  isOpen: boolean;
+  // eslint-disable-next-line no-unused-vars
+  setIsOpen: (isOpen: boolean) => void;
+  icon: React.ReactNode;
+  options: { id: number; name: string }[];
+  title: string;
+  subTitle: string;
+  selectedOption: string;
+  // eslint-disable-next-line no-unused-vars
+  setSelectedOption: (option: string) => void;
+}
+
+const DateTimeDropDown = ({
+  isOpen,
+  setIsOpen,
+  icon,
+  options,
+  title,
+  subTitle,
+  selectedOption,
+  setSelectedOption,
+}: DateTimeDropDownProps) => {
+  const onItemClick = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <DropDown handleClose={() => setIsOpen(false)}>
+      <DropDown.Trigger onClick={() => setIsOpen(!isOpen)}>
+        <div className='w-[150px] bg-white rounded-[8px] flex flex-row gap-2 items-center border border-lightBlueGray cursor-pointer relative p-2 justify-between'>
+          <div className='flex flex-row gap-2 items-center'>
+            {icon}
+            <p>{selectedOption || title}</p>
+          </div>
+          <motion.div
+            animate={{
+              rotate: isOpen ? 90 : -90,
+            }}
+            transition={{ duration: 0.2 }}
+          >
+            <BackArrowIcon width={24} height={24} />
+          </motion.div>
+        </div>
+      </DropDown.Trigger>
+      <DropDown.Menu
+        isOpen={isOpen}
+        position='top-13 left-1/2'
+        className='absolute left-1/2 -translate-x-1/2 bg-white rounded-[8px] flex flex-col gap-5 border border-lightBlueGray z-[9999] shadow-lg max-h-60 overflow-y-auto p-4 w-[150px]'
+      >
+        <p className='text-sm text-lightBlueGray font-medium'>{subTitle}</p>
+        <div className='flex flex-col gap-5 text-gray'>
+          {options.map((option) => (
+            <DropDown.Item key={option.id} onClick={() => onItemClick(option.name)}>
+              {option.name}
+            </DropDown.Item>
+          ))}
+        </div>
+      </DropDown.Menu>
+    </DropDown>
+  );
+};
+
+export default DateTimeDropDown;
