f3d209eddca4b111fd429ecf6f2e3898748c47e0
정찬원 <jcw0522@gachon.ac.kr>
2025-07-23 01:28:59 +0900

    refactory: 파일 구조 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Home from './Home';
 import Login from './Login';
-import Save from './save/Save';
+import Save from './Save';
 import Search from './Search';
 import Example from './Example';
 import SearchResult from './SearchResult';
