3630bd0d607227b3a9187570b81f71b345d7db67
정찬원 <jcw0522@gachon.ac.kr>
2025-07-31 03:56:21 +0900

    feat: 버튼에 opacity 애니메이션 적용

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -70,16 +70,20 @@ const Login = () => {
           <p className='font-medium text-[#6D7280] text-[13px] mt-5'>
             나의 인사이트, 잊어도 쉽게 찾아낼 수 있어야 하니까
           </p>
-
-          <div className='mt-15 w-full flex min-w-[330px]'>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+            className='mt-10 w-full flex justify-start'
+          >
             <Button
               onClick={handleKakaoLogin}
               icon={<KakaoLogoIcon width={18} height={18} className='flex mr-2' />}
-              className='bg-[#FEE500] font-semibold py-[14px] text-black hover:brightness-90 transition flex items-center justify-center rounded-[6px] shadow w-full'
+              className='bg-[#FEE500] font-semibold py-[14px] text-black hover:brightness-90 transition flex items-center justify-center rounded-[6px] shadow w-[90%] max-w-[320px]'
             >
               카카오로 로그인
             </Button>
-          </div>
+          </motion.div>
         </div>
       </div>
       <div className='absolute bottom-0 left-0 w-full h-70 bg-gradient-to-t from-blue-200 to-transparent pointer-events-none' />
