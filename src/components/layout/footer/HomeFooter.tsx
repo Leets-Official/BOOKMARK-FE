c2c7cd01992918b4be1110aabe7084315fd92de5
Donghyeon Shin <shindong0321@gmail.com>
2025-07-31 02:22:25 +0900

    fix : HomeFooter 링크 추가가 1440px에서 스크롤이 잠길 때 이동되는 현상 수정

--- src/components/layout/footer/HomeFooter.tsx
+++ src/components/layout/footer/HomeFooter.tsx
@@ -15,14 +15,7 @@ const HomeFooter = ({ isFixedBar }: HomeFooterProps) => {
   const scrollBarWidth = useAtomValue(scrollBarWidthAtom);
 
   return (
-    <div
-      className={clsx(
-        'fixed bottom-0 max-w-[1440px] w-full z-20',
-        scrollBarWidth > 0
-          ? `left-[calc(50%-${scrollBarWidth}px)]` // 스크롤바 너비만큼 왼쪽으로 조정
-          : 'left-1/2 transform -translate-x-1/2',
-      )}
-    >
+    <div className={clsx('fixed bottom-0 max-w-[1440px] w-full z-20')}>
       <div className='flex items-center px-8 sm:px-18 md:px-30 xl:px-38 mb-10'>
         {isFixedBar && (
           <motion.div
@@ -46,17 +39,22 @@ const HomeFooter = ({ isFixedBar }: HomeFooterProps) => {
             />
           </motion.div>
         )}
-        <button
-          onClick={() => navigate('save')}
-          className={clsx(
-            'bg-blue flex justify-center items-center sm:w-35 w-28 sm:h-17 h-13 text-white shadow-md',
-            'rounded-[100px] hover:brightness-75 transition gap-[10px] sm:text-[18px] text-[15px] border',
-            'border-[#B2CDFF] cursor-pointer ml-auto',
-          )}
+        <div
+          className='flex ml-auto'
+          style={{ marginRight: window.innerWidth > 1440 ? '0px' : `${scrollBarWidth}px` }}
         >
-          링크추가
-          <span className='sm:text-[30px] text-[24px]'>+</span>
-        </button>
+          <button
+            onClick={() => navigate('save')}
+            className={clsx(
+              'bg-blue flex justify-center items-center sm:w-35 w-28 sm:h-17 h-13 text-white shadow-md',
+              'rounded-[100px] hover:brightness-75 transition gap-[10px] sm:text-[18px] text-[15px] border',
+              'border-[#B2CDFF] cursor-pointer ml-auto',
+            )}
+          >
+            링크추가
+            <span className='sm:text-[30px] text-[24px]'>+</span>
+          </button>
+        </div>
       </div>
     </div>
   );
