67adf6f64f468274f1a97aeeb8944c31d43cf8f2
정찬원 <jcw0522@gachon.ac.kr>
2025-07-31 04:06:55 +0900

    chore: 버튼 너비 고정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -38,7 +38,7 @@ const Login = () => {
             나의 인사이트, 잊어도 쉽게 찾아낼 수 있어야 하니까
           </p>
         </div>
-        <div className='flex flex-col bg-loginBlue py-25 px-13 lg:pl-13 lg:pr-30'>
+        <div className='flex flex-col bg-loginBlue py-25 pl-13 pr-30'>
           <p className='text-white font-pretendard font-extrabold text-[40px] leading-[52px]'>
             로그인하고
             <br />
@@ -50,7 +50,7 @@ const Login = () => {
             <Button
               onClick={handleKakaoLogin}
               icon={<KakaoLogoIcon width={18} height={18} className='flex mr-2' />}
-              className='bg-[#FEE500] font-semibold text-15 py-[14px] text-black hover:brightness-90 transition flex items-center justify-center rounded-[6px] shadow w-[87%]'
+              className='bg-[#FEE500] font-semibold text-15 py-[14px] text-black hover:brightness-90 transition flex items-center justify-center rounded-[6px] w-[87%]'
             >
               카카오로 로그인
             </Button>
@@ -74,7 +74,7 @@ const Login = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 2 }}
-            className='mt-10 w-full flex justify-start'
+            className='mt-10 w-full flex min-w-[330px]'
           >
             <Button
               onClick={handleKakaoLogin}
