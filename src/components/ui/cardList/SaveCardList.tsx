90452f56b51fec69d26ca66b2de40613fe7602bf
Donghyeon Shin <shindong0321@gmail.com>
2025-08-07 00:17:29 +0900

    chore : Home page에 전체 보기 버튼 cursor pointer 적용

--- src/components/ui/cardList/SaveCardList.tsx
+++ src/components/ui/cardList/SaveCardList.tsx
@@ -79,7 +79,7 @@ const SaveCardList = () => {
           onClick={() => {
             navigate('/search-result');
           }}
-          className='sm:w-7/10 w-9/10 py-4 bg-white text-[15px] sm:text-base border-1 border-[#BCC0CC] rounded-[10px] active:brightness-95'
+          className='sm:w-7/10 w-9/10 py-4 bg-white text-[15px] sm:text-base border-1 border-[#BCC0CC] rounded-[10px] active:brightness-95 cursor-pointer'
         >
           전체보기
         </Button>
